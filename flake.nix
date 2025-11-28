edcc058cf41189d87e373c1096d8d05ab30b30ac
jmlb23 <rmmenosrf@riseup.net>
2025-11-28 17:36:04 +0100

    use postgres

--- flake.nix
+++ flake.nix
@@ -20,14 +20,30 @@
         # List common packages that exist on both platforms
         packages = with pkgs; [
           php83
-          mysql84
+          postgresql_18
           git
         ];
         shellHook = ''
-          PS1='\[\e[1m\e[35m\]\u\[\e[0m\] \[\e[1mat\]\[\e[0m\] \[\e[1m\e[34m\]\h\[\e[0m\] \[\e[1min\]\[\e[0m\] \[\e[1m\e[32m\]\w\[\e[0m\] with nix> '
-          PHP_VERSION=$(php -r 'echo phpversion();')
-          echo "Welcome to the $system environment!"
-          echo "with php version: $PHP_VERSION"
+            # 1. Setup local environment variables
+            export PGDATA="$PWD/.pg"
+            export PGHOST="$PWD/.pg" 
+            export PGLOG="$PWD/postgres.log"
+
+            # 2. Initialize database only if it doesn't exist
+            if [ ! -d "$PGDATA" ]; then
+                echo "Initializing postgresql database..."
+                initdb --auth=trust --no-locale --encoding=UTF8
+            fi
+
+            # 3. Start postgres only if not running
+            if ! pg_ctl status >/dev/null; then
+                pg_ctl start -l "$PGLOG" -o "-k '$PGHOST'"
+
+            fi
+            PS1='\[\e[1m\e[35m\]\u\[\e[0m\] \[\e[1mat\]\[\e[0m\] \[\e[1m\e[34m\]\h\[\e[0m\] \[\e[1min\]\[\e[0m\] \[\e[1m\e[32m\]\w\[\e[0m\] with nix> '
+            PHP_VERSION=$(php -r 'echo phpversion();')
+            echo "Welcome to the $system environment!"
+            echo "with php version: $PHP_VERSION"
         '';
       };
     });
